96b6a9910d5d508c13a6762036335598e530f61b
Timothy Brogan <timothy.brogan@mymail.champlain.edu>
2023-02-13 11:47:33 -0500

    Submitted secure-ssh.sh file

---
+++ SYS-265/linux/ubuntu/secure-ssh.sh
@@ -0,0 +1,6 @@
+# secure-ssh.sh
+# Author: tbrogan25
+# Creates a new SSH user using $1 parameter
+# Adds a public key from the local repo or curled from the remote repo
+# Removes roots ability to SSH in
+echo "TEST CODE"
