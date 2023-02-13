0ef904efd029de82993dfcc8c1d1190f31268121
tbrogan25 <timothy.brogan@mymail.champlain.edu>
2023-02-13 17:37:28 +0000

    Modified secure-ssh.sh Script

--- SYS-265/linux/ubuntu/secure-ssh.sh
+++ SYS-265/linux/ubuntu/secure-ssh.sh
@@ -4,3 +4,9 @@
 # Adds a public key from the local repo or curled from the remote repo
 # Removes roots ability to SSH in
 echo "TEST CODE"
+useradd -m -d /home/$1 -s /bin/bash $1
+mkdir /home/$1/.ssh
+cp /home/timothy/brogan-tech-journal/SYS-265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+chmod 700 /home/$1/.ssh
+chmod 600 /home/$1/.ssh/authorized_keys
+chown -R $1:$1 /home/$1/.ssh
